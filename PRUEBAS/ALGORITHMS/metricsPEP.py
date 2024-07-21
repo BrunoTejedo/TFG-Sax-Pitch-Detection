873adb53459e26f646b78b1884c3a46a18451c66
Bruno <brunojtm11@gmail.com>
2024-07-22 01:27:34 +0200

    more results included: boxplots and barplots

--- PRUEBAS/ALGORITHMS/metricsPEP.py
+++ PRUEBAS/ALGORITHMS/metricsPEP.py
@@ -10,7 +10,7 @@ import mir_eval.melody as mir
 import utilsPEP
 
 
-def analyze_folder(path, folder, results):
+def analyze_folder(path, folder, results, specific_results):
     level = folder[:6]
     algor = folder[11:15]
     
@@ -112,6 +112,12 @@ def analyze_folder(path, folder, results):
     results['RPA'][algor].extend(raw_pitch_accuracy)
     results['RCA'][algor].extend(raw_chroma_accuracy)
     results['OA'][algor].extend(overall_accuracy)
+    
+    specific_results['VR'][level][algor].extend(voicing_recall)
+    specific_results['VFA'][level][algor].extend(voicing_false_alarm)
+    specific_results['RPA'][level][algor].extend(raw_pitch_accuracy)
+    specific_results['RCA'][level][algor].extend(raw_chroma_accuracy)
+    specific_results['OA'][level][algor].extend(overall_accuracy)
 
     # Plots
     array = np.arange(1, len(voicing_recall) + 1)
@@ -152,15 +158,144 @@ def save_boxplots(results):
         plt.boxplot(box_plot_data, labels=algorithms)
         plt.ylabel("Score")
         plt.savefig(f'./boxplots/{metric}_algorithms.png')
+        
+def save_specific_boxplots(specific_results):
+    levels = ['LEVEL1', 'LEVEL2', 'LEVEL3']
+    algorithms = ['Yin', 'YinFFT', 'CREPE']
+    
+    for metric in ['VR', 'VFA', 'RPA', 'RCA', 'OA']:
+        for level in levels:
+            fig = plt.figure()
+            fig.suptitle(f'{metric}, {level}', fontsize=14, fontweight='bold')
+            box_plot_data = [
+                specific_results[metric][level][algorithm]
+                for algorithm in algorithms
+            ]
+            plt.boxplot(box_plot_data, labels=algorithms)
+            plt.ylabel('Score')
+            plt.savefig(f'./boxplots/{metric}_{level}.png')
+            plt.close()
 
+    for metric in ['VR', 'VFA', 'RPA', 'RCA', 'OA']:
+        for algorithm in algorithms:
+            fig = plt.figure()
+            fig.suptitle(f'{metric}, {algorithm}', fontsize=14, fontweight='bold')
+            box_plot_data = [
+                specific_results[metric][level][algorithm]
+                for level in levels
+            ]
+            plt.boxplot(box_plot_data, labels=levels)
+            plt.ylabel('Score')
+            plt.savefig(f'./boxplots/{metric}_{algorithm}.png')
+            plt.close()
+            
+def save_barplot(specific_results):
+    shutil.rmtree('./barplots', ignore_errors=True)
+    os.mkdir('./barplots')
+    
+    dictionary = {
+        'StaccatoEb': ['LEVEL1', 0, 24], 'LegatoBb': ['LEVEL1', 1, 24],
+        'LegatoC': ['LEVEL1', 2, 24], 'StaccatoAb': ['LEVEL1', 3, 24],
+        'StaccatoG': ['LEVEL1', 4, 24], 'StaccatoD': ['LEVEL1', 5, 24],
+        'LegatoD': ['LEVEL1', 6, 24], 'StaccatoC': ['LEVEL1', 7, 24],
+        'LegatoF#': ['LEVEL1', 8, 24], 'StaccatoF': ['LEVEL1', 9, 24],
+        'StaccatoC#': ['LEVEL1', 10, 24], 'LegatoB': ['LEVEL1', 11, 24],
+        'StaccatoBb': ['LEVEL1', 12, 24], 'LegatoF': ['LEVEL1', 13, 24],
+        'StaccatoB': ['LEVEL1', 14, 24], 'StaccatoA': ['LEVEL1', 15, 24],
+        'LegatoEb': ['LEVEL1', 16, 24], 'LegatoG': ['LEVEL1', 17, 24],
+        'LegatoC#': ['LEVEL1', 18, 24], 'LegatoAb': ['LEVEL1', 19, 24],
+        'StaccatoE': ['LEVEL1', 20, 24], 'StaccatoF#': ['LEVEL1', 21, 24],
+        'LegatoA': ['LEVEL1', 22, 24], 'LegatoE': ['LEVEL1', 23, 24],
+        'myIdeal': ['LEVEL2', 0, 6], 'turnaround': ['LEVEL2', 1, 6],
+        'GoodbyePorkPieHat': ['LEVEL2', 2, 6], 'secretlove': ['LEVEL2', 3, 6],
+        'SaborAmi': ['LEVEL2', 4, 6], 'timeWasMelo': ['LEVEL2', 5, 6],
+        'ColtraneSolo2ndTimeWas': ['LEVEL3', 0, 8], 'donnaLee': ['LEVEL3', 1, 8],
+        'zyryab': ['LEVEL3', 2, 8], 'ColtraneSolo1stTimeWas': ['LEVEL3', 3, 8],
+        'lickTimeWasCompo': ['LEVEL3', 4, 8], 'ColtraneSolo4thTimeWas': ['LEVEL3', 5, 8],
+        'ColtraneSolo3rdTimeWas': ['LEVEL3', 6, 8], 'airegin': ['LEVEL3', 7, 8],
+    }
+    
+    categories = ['Yin', 'YinFFT', 'CREPE']
+    n_categories = len(categories)
+    width = 0.15
+    
+    for audio in dictionary.keys():
+        VR = [
+            np.mean(specific_results['VR'][dictionary[audio][0]]['Yin'][dictionary[audio][1]::dictionary[audio][2]]),
+            np.mean(specific_results['VR'][dictionary[audio][0]]['YinFFT'][dictionary[audio][1]::dictionary[audio][2]]),
+            np.mean(specific_results['VR'][dictionary[audio][0]]['CREPE'][dictionary[audio][1]::dictionary[audio][2]])
+        ]
+        VFA = [
+            np.mean(specific_results['VFA'][dictionary[audio][0]]['Yin'][dictionary[audio][1]::dictionary[audio][2]]),
+            np.mean(specific_results['VFA'][dictionary[audio][0]]['YinFFT'][dictionary[audio][1]::dictionary[audio][2]]),
+            np.mean(specific_results['VFA'][dictionary[audio][0]]['CREPE'][dictionary[audio][1]::dictionary[audio][2]])
+        ]
+        RPA = [
+            np.mean(specific_results['RPA'][dictionary[audio][0]]['Yin'][dictionary[audio][1]::dictionary[audio][2]]),
+            np.mean(specific_results['RPA'][dictionary[audio][0]]['YinFFT'][dictionary[audio][1]::dictionary[audio][2]]),
+            np.mean(specific_results['RPA'][dictionary[audio][0]]['CREPE'][dictionary[audio][1]::dictionary[audio][2]])
+        ]
+        RCA = [
+            np.mean(specific_results['RCA'][dictionary[audio][0]]['Yin'][dictionary[audio][1]::dictionary[audio][2]]),
+            np.mean(specific_results['RCA'][dictionary[audio][0]]['YinFFT'][dictionary[audio][1]::dictionary[audio][2]]),
+            np.mean(specific_results['RCA'][dictionary[audio][0]]['CREPE'][dictionary[audio][1]::dictionary[audio][2]])
+        ]
+        OA = [
+            np.mean(specific_results['OA'][dictionary[audio][0]]['Yin'][dictionary[audio][1]::dictionary[audio][2]]),
+            np.mean(specific_results['OA'][dictionary[audio][0]]['YinFFT'][dictionary[audio][1]::dictionary[audio][2]]),
+            np.mean(specific_results['OA'][dictionary[audio][0]]['CREPE'][dictionary[audio][1]::dictionary[audio][2]])
+        ]
+
+        ind = np.arange(n_categories)
+
+        fig, ax = plt.subplots()
+
+        ax.bar(ind - 2 * width, VR, width, label='VR', color='skyblue')
+        ax.bar(ind - width, VFA, width, label='VFA', color='lightgreen')
+        ax.bar(ind, RPA, width, label='RPA', color='salmon')
+        ax.bar(ind + width, RCA, width, label='RCA', color='orange')
+        ax.bar(ind + 2 * width, OA, width, label='OA', color='purple')
+
+        ax.set_title(audio)
+        ax.set_xlabel('Algorithms')
+        ax.set_ylabel('Score')
+        ax.set_xticks(ind)
+        ax.set_xticklabels(categories)
+        ax.legend()
+    
+        plt.savefig(f'./barplots/{audio}_metrics.png')
+        plt.close()
 
 def main():
     results = {
-        'VR': {'LEVEL1': [], 'LEVEL2': [], 'LEVEL3': [], 'Yin': [], 'YinFFT': [], 'CREPE': []},
-        'VFA': {'LEVEL1': [], 'LEVEL2': [], 'LEVEL3': [], 'Yin': [], 'YinFFT': [], 'CREPE': []},
-        'RPA': {'LEVEL1': [], 'LEVEL2': [], 'LEVEL3': [], 'Yin': [], 'YinFFT': [], 'CREPE': []},
-        'RCA': {'LEVEL1': [], 'LEVEL2': [], 'LEVEL3': [], 'Yin': [], 'YinFFT': [], 'CREPE': []},
-        'OA': {'LEVEL1': [], 'LEVEL2': [], 'LEVEL3': [], 'Yin': [], 'YinFFT': [], 'CREPE': []},
+        'VR': {'LEVEL1': [], 'LEVEL2': [], 'LEVEL3': [], 'Yin': [],
+               'YinFFT': [], 'CREPE': []},
+        'VFA': {'LEVEL1': [], 'LEVEL2': [], 'LEVEL3': [], 'Yin': [],
+                'YinFFT': [], 'CREPE': []},
+        'RPA': {'LEVEL1': [], 'LEVEL2': [], 'LEVEL3': [], 'Yin': [],
+                'YinFFT': [], 'CREPE': []},
+        'RCA': {'LEVEL1': [], 'LEVEL2': [], 'LEVEL3': [], 'Yin': [],
+                'YinFFT': [], 'CREPE': []},
+        'OA': {'LEVEL1': [], 'LEVEL2': [], 'LEVEL3': [], 'Yin': [],
+               'YinFFT': [], 'CREPE': []},
+    }
+    
+    specific_results = {
+        'VR': {'LEVEL1': {'Yin': [], 'YinFFT': [], 'CREPE': []},
+               'LEVEL2': {'Yin': [], 'YinFFT': [], 'CREPE': []},
+               'LEVEL3': {'Yin': [], 'YinFFT': [], 'CREPE': []}},
+        'VFA': {'LEVEL1': {'Yin': [], 'YinFFT': [], 'CREPE': []},
+                'LEVEL2': {'Yin': [], 'YinFFT': [], 'CREPE': []},
+                'LEVEL3': {'Yin': [], 'YinFFT': [], 'CREPE': []}},
+        'RPA': {'LEVEL1': {'Yin': [], 'YinFFT': [], 'CREPE': []},
+                'LEVEL2': {'Yin': [], 'YinFFT': [], 'CREPE': []},
+                'LEVEL3': {'Yin': [], 'YinFFT': [], 'CREPE': []}},
+        'RCA': {'LEVEL1': {'Yin': [], 'YinFFT': [], 'CREPE': []},
+                'LEVEL2': {'Yin': [], 'YinFFT': [], 'CREPE': []},
+                'LEVEL3': {'Yin': [], 'YinFFT': [], 'CREPE': []}},
+        'OA': {'LEVEL1': {'Yin': [], 'YinFFT': [], 'CREPE': []},
+               'LEVEL2': {'Yin': [], 'YinFFT': [], 'CREPE': []},
+               'LEVEL3': {'Yin': [], 'YinFFT': [], 'CREPE': []}},
     }
 
     for folder in os.listdir('./RESULTS'):
@@ -169,10 +304,13 @@ def main():
         shutil.rmtree(os.path.join(path, 'metrics'), ignore_errors=True)
         os.mkdir(os.path.join(path, 'metrics'))
 
-        analyze_folder(path, folder, results)
-
+        analyze_folder(path, folder, results, specific_results)
+    
     print("Saving metrics boxplots ...")
     save_boxplots(results)
+    save_specific_boxplots(specific_results)
+    print("Saving metrics barplots ...")
+    save_barplot(specific_results)
 
 
 if __name__ == '__main__':
